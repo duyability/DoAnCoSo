09ef9a954da27ce530628b964807eef50ffb0fdf
PhamMinhDuc <dwebtech.net@gmail.com>
2019-05-30 19:18:41 +0700

    PHAM MINH DUC
    setup new code

---
+++ python_job/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!A:\DoAnCoSo\DoAnCoSo\python_job\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ python_job/vlance/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VlanceConfig(AppConfig):
+    name = 'vlance'
